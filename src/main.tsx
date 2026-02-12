52903bf016ac1ddf3c39ae51cf903a4d0a8ffde4
Diya <desaivdiya@gmail.com>
2026-02-11 22:52:02 -0800

    Changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './styles/globals.css';
 import App from './App';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Support } from './components/resources/Support';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
